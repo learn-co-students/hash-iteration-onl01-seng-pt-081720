64527eb38d4499ed8a73cac3ce1ede1c130f9e80
Areeba Zahir <areebazahir3@gmail.com>
2020-08-30 06:31:58 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,11 +7,12 @@ passengers = {
 }
 
 def select_winner(passengers)
-   winner = ""
-   passengers.each { |suite, name|
-      if suite == :suite_a && name.start_with?("A")
-      winner = name 
-      end
-   }
-   winner
+  winner = ""
+  passengers.each do |suite, name|
+    if suite == :suite_a && name.start_with?("A")
+      winner = name
+    end
+  end
+
+  winner 
 end
